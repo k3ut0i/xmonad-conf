48354a673fa03e92677959ee1056c899574700f6
k3ut0i <k3ut0i@gmail.com>
2015-03-03 15:02:38 +0530

    working with additionalKeys;change from master
    Signed-off-by: k3ut0i <k3ut0i@gmail.com>

--- xmonad.hs
+++ xmonad.hs
@@ -4,13 +4,16 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
-main = xmonad $ defaultConfig
+main = xmonad $ myConfig `additionalKeys` myKeys
+       
+
+myConfig = defaultConfig
         {manageHook = manageDocks <+> manageHook defaultConfig,
         layoutHook = avoidStruts  $  layoutHook defaultConfig,
         terminal = "urxvt",
         borderWidth = 3
-        }`additionalKeys`
-        [
+        }
+myKeys =  [
         ((mod4Mask , xK_z), spawn "xscreensaver-command -lock"),
         ((mod4Mask , xK_p), spawn "scrot \"%Y-%m-%d-%s_$wx$h.png\" -e \"mv $f ~/Pictures/Scrots/\""),
         ((0, xK_Print), spawn "scrot")
