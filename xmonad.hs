75f82892c7fd65edbe57a7784d98597b7c95870e
k3ut0i <k3ut0i@gmail.com>
2015-03-03 14:41:45 +0530

    Some Changes;mod mask not working

--- xmonad.hs
+++ xmonad.hs
@@ -4,14 +4,25 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
-main = xmonad $ defaultConfig
-        {manageHook = manageDocks <+> manageHook defaultConfig,
+import qualified Data.Map as M
+main = xmonad myConfig
+
+myConfig = defaultConfig
+        {
+        --Hooks ans layouts
+        manageHook = manageDocks <+> manageHook defaultConfig,
         layoutHook = avoidStruts  $  layoutHook defaultConfig,
+        --Simple Variables
         terminal = "urxvt",
-        borderWidth = 3
-        }`additionalKeys`
-        [
+        borderWidth = 4,
+        --Bindings
+        keys = myKeys
+        }
+
+myKeys conf@(XConfig{XMonad.modMask = mod1Mask})= M.fromList [
         ((mod4Mask , xK_z), spawn "xscreensaver-command -lock"),
         ((mod4Mask , xK_p), spawn "scrot \"%Y-%m-%d-%s_$wx$h.png\" -e \"mv $f ~/Pictures/Scrots/\""),
         ((0, xK_Print), spawn "scrot")
         ]
+
+
