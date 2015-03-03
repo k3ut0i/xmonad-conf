561b9e3e4612860ca1372b0805f4145e02dfc9c9
k3ut0i <k3ut0i@gmail.com>
2015-03-03 12:59:17 +0530

    Basic Working Conf.Must add bar
    Signed-off-by: k3ut0i <k3ut0i@gmail.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# xmonad-conf
+\main_page
+    Configuration files for xmonad with xmobar
+    Most initial config from John Goerzen 
+    Changed according to need
+    default keys have mod1Mask
+        where as custom changed ones have mod4Mask 
--- xmonad.hs
+++ xmonad.hs
@@ -4,8 +4,14 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
-main = xmonad defaultConfig
-        {
-    terminal = "urxvt",
-    borderWidth = 3
-        }
+main = xmonad $ defaultConfig
+        {manageHook = manageDocks <+> manageHook defaultConfig,
+        layoutHook = avoidStruts  $  layoutHook defaultConfig,
+        terminal = "urxvt",
+        borderWidth = 3
+        }`additionalKeys`
+        [
+        ((mod4Mask , xK_z), spawn "xscreensaver-command -lock"),
+        ((mod4Mask , xK_p), spawn "scrot \"%Y-%m-%d-%s_$wx$h.png\" -e \"mv $f ~/Pictures/Scrots/\""),
+        ((0, xK_Print), spawn "scrot")
+        ]
